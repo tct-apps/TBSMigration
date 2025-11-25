d9bd47fb5967e2579f42564d837d6b9203541559
Madi <madihah@gohub.com.my>
2025-11-25 18:01:18 +0800

    Create StateResponseModel.cs

---
+++ PushTripTOS/Model/State/StateResponseModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace PushMasterTOS.Model
+{
+    [XmlRoot(ElementName = "stateInsertResponse ", Namespace = "http://tos.org/")]
+    public class StateResponseModel
+    {
+        [XmlElement(ElementName = "stateInsertResult")]
+        public StateInsertResult StateInsertResult { get; set; }
+    }
+
+    public class StateInsertResult
+    {
+        [XmlElement(ElementName = "code")]
+        public int Code { get; set; }
+
+        [XmlElement(ElementName = "msg")]
+        public string Msg { get; set; }
+    }
+}
