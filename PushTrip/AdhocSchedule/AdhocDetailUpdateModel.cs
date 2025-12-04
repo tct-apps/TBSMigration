cdc638acac2f97fa0bd6f1ace22976e643f52fa8
Ain <ain.emylia@gohub.com.my>
2025-12-04 11:55:19 +0800

    Create AdhocDetailUpdateModel.cs

---
+++ PushTrip/AdhocSchedule/AdhocDetailUpdateModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushTrip.AdhocSchedule
+{
+    public class AdhocDetailUpdateModel
+    {
+        public class Request
+        {
+            public string TripNo { get; set; }
+            public string GateNo { get; set; }
+            public string GateNo2 { get; set; }
+            public DateTime TripDate { get; set; }
+            public string AdhocId { get; set; }
+            public int Posi { get; set; }
+            public string CompanyCode { get; set; }
+        }
+    }
+}
